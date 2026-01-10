3a1fabf9d69a132c138b24988f98ab9594251358
APZelos <a.p.zelos@gmail.com>
2026-01-10 16:36:30 +0200

    Fix formatting

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,17 @@ import {defineConfig} from "eslint/config"
 import tseslint from "typescript-eslint"
 
 export default defineConfig(
-  {ignores: ["**/dist/**", "**/node_modules/**", "src/test/convex/_generated", "test/convex/_generated", "**/vitest.config.ts", "**/tsup.config.ts", "vitest.workspace.ts"]},
+  {
+    ignores: [
+      "**/dist/**",
+      "**/node_modules/**",
+      "src/test/convex/_generated",
+      "test/convex/_generated",
+      "**/vitest.config.ts",
+      "**/tsup.config.ts",
+      "vitest.workspace.ts",
+    ],
+  },
   {
     extends: [
       js.configs.recommended,
