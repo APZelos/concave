7289fc16058413a78012f34c5d6e0918aa3d9a6b
APZelos <a.p.zelos@gmail.com>
2026-01-10 17:02:43 +0200

    Simplify concave-model build

--- packages/model/tsup.config.ts
+++ packages/model/tsup.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from "tsup"
 
 export default defineConfig({
   entry: ["src/index.ts", "src/testing/index.ts"],
-  dts: false,
+  dts: true,
   sourcemap: true,
   clean: true,
   format: ["esm"],
